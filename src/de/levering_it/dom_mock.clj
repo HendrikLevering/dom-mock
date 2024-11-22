b593ba214380136a4c46da29a668e95f34160ef5
HendrikLevering <94710021+HendrikLevering@users.noreply.github.com>
2024-11-22 14:13:31 +0100

    skeleton

---
+++ src/de/levering_it/dom_mock.clj
@@ -0,0 +1,6 @@
+(ns de.levering-it.dom-mock)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/de/levering_it/dom_mock_test.clj
@@ -0,0 +1,7 @@
+(ns de.levering-it.dom-mock-test
+  (:require [clojure.test :refer :all]
+            [de.levering-it.dom-mock :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
